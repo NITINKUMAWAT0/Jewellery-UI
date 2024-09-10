1866135e949d7e00cabebe774415876aeef326ec
NITINKUMAWAT0 <nitin.kumawat.jaipur.in@gmail.com>
2024-09-10 20:48:41 +0530

    Changed the name of Carousel folder

--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import './Home.scss'
-import Carousel from '../../Components/Carousel/Carousel'
+import React from 'react';
+import './Home.scss';
+import Carousel from '../../Components/Carousel_Component/Carousel';
 const Home = () => {
   return (
     <div className='home'>
-     
       <Carousel />
-      
     </div>
   )
 }
