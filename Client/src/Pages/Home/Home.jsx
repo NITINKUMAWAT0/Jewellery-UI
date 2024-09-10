6b2af89adc8b073f92e174f523a36ada4f146510
Nikhil Agarwal <153534255+nikhilgodfather@users.noreply.github.com>
2024-09-10 19:57:18 +0530

    Update Home.jsx

--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Home.scss'
-import Carousel from '../../Components/Carousel /Carousel'
+import Carousel from '../../Components/Carousel/Carousel'
 const Home = () => {
   return (
     <div className='home'>
