a9986406e3f0b9a328fa3ed653f8ad31f8b66d80
NITINKUMAWAT0 <nitin.kumawat.jaipur.in@gmail.com>
2024-09-12 10:10:08 +0530

    .

---
+++ Client/src/Components/Home_Products/HomeProducts.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HomeProducts = () => {
+    return (
+        <>
+        {/* Upper Carouel */}
+        <div className="upper-carousel">
+            <h1>Most Loved on Jwells</h1>
+
+            {/* Carousel starting */}
+        </div>
+
+        {/* Lower Carouel */}
+        <div className="lower-carousel">
+            <h1>Gifts That Say It All</h1>
+        </div>
+
+        </>
+    )
+}
+
+export default HomeProducts
--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.scss';
 import Carousel from '../../Components/Carousel_Component/Carousel';
+import HomeProducts from '../../Components/Home_Products/HomeProducts';
 const Home = () => {
   return (
     <div className='home'>
       <Carousel />
+      <HomeProducts/>
     </div>
   )
 }
